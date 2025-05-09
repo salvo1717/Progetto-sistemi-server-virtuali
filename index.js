929b5cb8c040328dc77652c90e8aa7dabe1189f4
Salvatore Frattaruolo <160015236+salvo1717@users.noreply.github.com>
2025-05-09 19:20:13 +0200

    Delete index.js

--- index.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    document.getElementById("dimenticata").addEventListener("click", function() {
-        window.location.assign("dimenticata.html");
-    });
-    document.getElementById("crea").addEventListener("click", function() {
-        window.location.assign("crea.html");
-    });
-});
