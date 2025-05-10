2b7b34e2ba6e13a3cb5270e38e03cc689664817e
Salvatore Frattaruolo <160015236+salvo1717@users.noreply.github.com>
2025-05-10 18:46:57 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', function() {
     document.getElementById("dimenticata").addEventListener("click", function() {
-        window.location.assign("dimenticata.html");
+        window.location.assign("dimenticata.php");
     });
     document.getElementById("crea").addEventListener("click", function() {
         window.location.assign("crea.html");
